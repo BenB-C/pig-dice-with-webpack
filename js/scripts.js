62ba1e87fa73824b073bac7c9eadb3c86d206829
ben bennett-cauchon, Erin Deuel and marina poltorak <erin@>
2019-06-19 11:51:26 -0700

    add rollDie prototype method and some test code

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,12 @@ function Die() {
 // Randomly assigns a number between 1 and 6 to the die's number
 Die.prototype.roll = function() {
   this.number = Math.floor(Math.random()*6)+1;
-  console.log(this.number);
+  return this.number;
 }
 
 function Player(name) {
   this.name = name;
   this.score = 0;
-
 }
 
 function Game () {
@@ -25,13 +24,28 @@ function Game () {
   this.die = new Die ();
 }
 
+Game.prototype.rollDie = function() {
+  this.turnTotal += this.die.roll();
+  var currentPlayer = this.players[this.playerUpIndex];
+  console.log(currentPlayer.name + " rolled a " + this.die.number);
+  console.log("turnTotal", this.turnTotal);
+  console.log("currentPlayer", currentPlayer);
+}
+
+Game.prototype.addToScore(playerIndex) {
+
+}
+
+// Create new game
 var game = new Game ();
 console.log(game);
 
-var die = new Die(1);
-console.log("die.number", die.number);
-die.roll();
-console.log("die.number", die.number);
-
+// Create players and add them to the game
 var player1 = new Player("Marina");
 var player2 = new Player("Erin");
+(game.players).push(player1);
+(game.players).push(player2);
+
+// Roll the die
+game.rollDie();
+game.rollDie();
